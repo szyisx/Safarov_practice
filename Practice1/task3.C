8b3a461af1e93f68ec94f5bccaf320c68b25bb2b
Szyisx <buldakoff.artiom2017@gmail.com>
2025-03-12 13:27:44 +0300

    task3

--- Practice1/task3.C
+++ Practice1/task3.C
@@ -6,7 +6,17 @@ int main(){
 
     setlocale(LC_ALL, "Russian");
 
-    
+    char symbol;
+
+    printf("Введите символ: ");
+    scanf("%c", &symbol);
+
+    if (symbol >= 'A' && symbol <= 'Z'){
+        printf("символ входит в диапозон");
+    }
+    else {
+        printf("символ не входит в диапозон");
+    }
 
     return 0;
 }
